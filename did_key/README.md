ea6f6ac24d5de32c5ff1e5290a7d9c6a56a19452
Daniel Mader <daniel.mader@impierce.com>
2024-02-12 22:30:09 +0100

    chore: insert verification method manually from generated jwk, add links to specs

--- did_key/README.md
+++ did_key/README.md
@@ -4,3 +4,4 @@ Implements identity.rs resolver interface for consuming did:key DIDs.
 Storage interface for producing did:key DIDs.
 
 Using [this implementation](https://github.com/decentralized-identity/did-key.rs) for did:key.
+Find the spec [here](https://w3c-ccg.github.io/did-method-key/).
--- did_key/src/producer/mod.rs
+++ did_key/src/producer/mod.rs
@@ -1,22 +1,18 @@
+use std::io::Error;
+
 use identity_iota::{
-    core::{FromJson, ToJson},
-    credential::Jws,
+    core::ToJson,
     did::{CoreDID, DID},
-    document::{self, CoreDocument},
-    storage::{JwkDocumentExt, JwkStorage, KeyIdStorage, KeyType, Storage},
-    verification::{
-        jwk::{Jwk, JwkParamsOkp},
-        jws::JwsAlgorithm,
-        MethodScope,
-    },
+    document::CoreDocument,
+    storage::{JwkDocumentExt, JwkStorage, KeyId, KeyIdStorage, KeyType, Storage},
+    verification::{jws::JwsAlgorithm, MethodScope, VerificationMethod},
 };
-use iota_sdk::utils::serde;
 use ssi_dids::{DIDMethod, Source};
 
 pub async fn produce_did_from_key<K, I>(
     storage: Storage<K, I>,
     password: &str,
-    key_id: &str,
+    key_id: &KeyId,
 ) -> std::result::Result<CoreDocument, std::io::Error>
 where
     K: JwkStorage,
@@ -24,60 +20,95 @@ where
 {
     println!(
         "Producing DID for key_id=[{:?}] from storage=[{:?}] ...",
-        key_id, "TODO_get_name"
+        key_id.as_str(),
+        "TODO_get_name"
     );
 
-    // TODO: read public key of JWK from storage
-    // ...
+    let exists = storage.key_storage().exists(key_id).await.unwrap();
 
-    // TODO (remove): this simulates we got the public key from the storage
-    let mut jwk = Jwk::from_params(
-        JwkParamsOkp::from_json(
-            r#"{
-                "crv": "Ed25519",
-                "x": "uhxVazAiEvAsPkVzfe3xeTpmNFBP4rEw5DBHu3lvYCg"
-            }"#,
-        )
-        .unwrap(),
-    );
-    jwk.set_alg(JwsAlgorithm::EdDSA.name());
-    println!("JWK: {}", jwk.to_public().to_json().unwrap());
+    if !exists {
+        return Err(Error::other(format!("Key with id=[{}] does not exist", key_id)));
+    }
 
-    // Create did:key from JWK
-    let did_str = did_method_key::DIDKey
-        .generate(&Source::Key(&serde_json::from_str(&jwk.to_json().unwrap()).unwrap()))
-        .unwrap();
+    // // TODO (remove): this simulates we got the public key from the storage
+    // let mut jwk = Jwk::from_params(
+    //     JwkParamsOkp::from_json(
+    //         r#"{
+    //             "crv": "Ed25519",
+    //             "x": "uhxVazAiEvAsPkVzfe3xeTpmNFBP4rEw5DBHu3lvYCg"
+    //         }"#,
+    //     )
+    //     .unwrap(),
+    // );
+    // jwk.set_alg(JwsAlgorithm::EdDSA.name());
+    // println!("JWK: {}", jwk.to_public().to_json().unwrap());
 
-    // println!("DID: {}", did_str);
+    // // Create did:key from JWK
+    // let did_str = did_method_key::DIDKey
+    //     .generate(&Source::Key(&serde_json::from_str(&jwk.to_json().unwrap()).unwrap()))
+    //     .unwrap();
 
-    let controller = CoreDID::parse(&did_str).unwrap();
+    // Use temporary DID string to satisfy the document builder
+    let temp_did_str = "did:foo:bar";
 
-    let mut document = CoreDocument::builder(Default::default())
-        .id(controller.clone())
+    let temp_controller = CoreDID::parse(&temp_did_str).unwrap();
+
+    let mut temp_document = CoreDocument::builder(Default::default())
+        .id(temp_controller.clone())
         .build()
         .unwrap();
 
-    // println!("{}", document.to_json_pretty().unwrap());
+    // temp_document.insert_method(method, scope)
 
     // Insert new verification method
-    document
+    temp_document
         .generate_method(
             &storage,
             KeyType::from_static_str("Ed25519"),
             JwsAlgorithm::EdDSA,
-            Some(controller.method_id()),
+            Some(temp_controller.method_id()),
             MethodScope::VerificationMethod,
         )
         .await
         .unwrap();
 
+    let temp_verification_method = temp_document.verification_method().first().unwrap().to_owned();
+    let public_key_jwk = temp_verification_method.data().public_key_jwk().unwrap();
+    println!(
+        "(generated) public_key_jwk: {}",
+        public_key_jwk.to_json_pretty().unwrap()
+    );
+
+    let did_str = did_method_key::DIDKey
+        .generate(&Source::Key(
+            &serde_json::from_str(&public_key_jwk.to_json().unwrap()).unwrap(),
+        ))
+        .unwrap();
+    let did = CoreDID::parse(&did_str).unwrap();
+    println!("DID: {}", did);
+
+    // first_verification_method
+    //     .set_id(DIDUrl::parse(format!("{}#{}", did, did.method_id())).unwrap())
+    //     .unwrap();
+
+    let verification_method =
+        VerificationMethod::new_from_jwk(did.clone(), public_key_jwk.clone(), Some(did.method_id())).unwrap();
+    // println!("VerificationMethod: {}", verification_method.to_json_pretty().unwrap());
+
+    let document = CoreDocument::builder(Default::default())
+        .id(did)
+        .verification_method(verification_method)
+        .build()
+        .unwrap();
     println!("DID Document: {}", document.to_json_pretty().unwrap());
+
     Ok(document)
 }
 
 #[cfg(test)]
 pub mod tests {
     use crypto::signatures::ed25519::SecretKey;
+    use did_key::{Config, DIDCore};
     use identity_iota::core::{FromJson, ToJson};
     use identity_iota::storage::{JwkMemStore, KeyIdMemstore};
     use identity_iota::verification::jwk::{EdCurve, Jwk, JwkParamsOkp};
@@ -94,7 +125,22 @@ pub mod tests {
     async fn test_mem_storage() {
         type MemStorage = Storage<JwkMemStore, KeyIdMemstore>;
         let storage: MemStorage = MemStorage::new(JwkMemStore::new(), KeyIdMemstore::new());
-        let document = produce_did_from_key(storage, "foo", "bar").await.unwrap();
+
+        let mut jwk = Jwk::from_params(
+            JwkParamsOkp::from_json(
+                r#"{
+                    "crv": "Ed25519",
+                    "x": "uhxVazAiEvAsPkVzfe3xeTpmNFBP4rEw5DBHu3lvYCg",
+                    "d":"AGj4AHCRm6ApGng2-9F7HMCtlO19x4DhrAg5_71t2CQ"
+                }"#,
+            )
+            .unwrap(),
+        );
+        jwk.set_alg(JwsAlgorithm::EdDSA.name());
+
+        let key_id = storage.key_storage().insert(jwk).await.unwrap();
+
+        let document = produce_did_from_key(storage, "foo", &key_id).await.unwrap();
 
         assert_eq!(
             document.id().to_string(),
@@ -109,11 +155,12 @@ pub mod tests {
         println!("====== Preparing Stronghold");
 
         // Create stronghold
-        let stronghold_secret_manager = StrongholdSecretManager::builder()
+        let stronghold = StrongholdSecretManager::builder()
             .password(Password::from("secure_password".to_owned()))
             .build(random_stronghold_path())
             .unwrap();
-        let stronghold_storage = StrongholdStorage::new(stronghold_secret_manager);
+
+        let stronghold_storage = StrongholdStorage::new(stronghold);
 
         // Generate key
         let private_key = SecretKey::generate().unwrap();
@@ -129,7 +176,8 @@ pub mod tests {
         let mut jwk = Jwk::from_params(params);
         jwk.set_alg(JwsAlgorithm::EdDSA.name());
 
-        // TODO: temporarily overwrite with known static values
+        // TODO: temporarily overwrite with known static values for better assertions
+        // since we can pre-calculate the expected did:key
         jwk = Jwk::from_params(
             JwkParamsOkp::from_json(
                 r#"{
@@ -144,14 +192,26 @@ pub mod tests {
 
         println!("JWK: {}", jwk.params().to_json().unwrap());
         // Insert into stronghold
-        let key_id = stronghold_storage.insert(jwk).await.unwrap();
+        let key_id = stronghold_storage.insert(jwk.clone()).await.unwrap();
 
         let storage = Storage::new(stronghold_storage.clone(), stronghold_storage);
         println!("====== Done");
 
-        let document = produce_did_from_key(storage, "secure_password", key_id.as_str())
-            .await
+        let expected_did = did_method_key::DIDKey
+            .generate(&Source::Key(&serde_json::from_str(&jwk.to_json().unwrap()).unwrap()))
             .unwrap();
+        println!("Expected DID: {}", expected_did);
+
+        let document = produce_did_from_key(storage, "secure_password", &key_id).await.unwrap();
+
+        // println!(
+        //     "{}",
+        //     did_key::resolve(&expected_did)
+        //         .unwrap()
+        //         .get_did_document(Config::default())
+        //         .to_json_pretty()
+        //         .unwrap()
+        // );
 
         assert_eq!(
             document.id().to_string(),
