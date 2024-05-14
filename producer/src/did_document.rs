3203936193fde0b7837cdc20f491cd535630dd79
Daniel Mader <daniel.mader@impierce.com>
2024-05-15 00:28:48 +0200

    refactor: implement `Display` for `DidMethod`

--- producer/src/did_document.rs
+++ producer/src/did_document.rs
@@ -3,31 +3,29 @@ use identity_iota::iota::IotaDID;
 use serde::{Deserialize, Serialize};
 use shared::error::ProducerError;
 use shared::JwkStorageWrapper;
-use std::string::ToString;
-use strum::Display;
 
 use crate::SecretManager;
 
-#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Display)]
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 pub enum DidMethod {
     #[serde(rename = "did:jwk")]
-    #[strum(serialize = "did:jwk")]
     Jwk,
     #[serde(rename = "did:key")]
-    #[strum(serialize = "did:key")]
     Key,
     #[serde(rename = "did:web")]
-    #[strum(serialize = "did:web")]
     Web,
     #[serde(rename = "did:iota:rms")]
-    #[strum(serialize = "did:iota:rms")]
     ShimmerTestnet,
     #[serde(rename = "did:iota:smr")]
-    #[strum(serialize = "did:iota:smr")]
     Shimmer,
     #[serde(rename = "did:iota")]
-    #[strum(serialize = "did:iota")]
     IotaMainnet,
+}
+
+impl std::fmt::Display for DidMethod {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", serde_json::json!(self).as_str().ok_or(std::fmt::Error)?)
+    }
 }
 
 impl SecretManager {
