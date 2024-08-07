856040401208ce23c2b9bb9d2eb7168948cc1d63
Daniel Mader <daniel.mader@impierce.com>
2024-08-07 18:16:04 +0200

    chore: export `InMemoryCache`

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub use consumer::resolver::Resolver;
+pub use producer::cache::InMemoryCache;
 pub use producer::did_document::DidMethod;
 pub use producer::MethodSpecificParameters;
 pub use producer::SecretManager;
